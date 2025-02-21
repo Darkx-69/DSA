9b737ae7ad5e3ea06d3aed0368fad4f83d28f2d2
Darkx-69 <umaaditya7809@gmail.com>
2025-02-21 13:55:54 +0530

    new

--- Sorting/Merge_sort.c
+++ Sorting/Merge_sort.c
@@ -1,72 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
-void merge(int arr[],int l,int m,int r)
-{
-    int n1=m-l+1;
-    int n2=r-m;
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to merge two subarrays of arr[]
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;  // Size of left subarray
+    int n2 = r - m;      // Size of right subarray
+
+    // Allocate memory for temporary arrays
     int *L1 = (int *)malloc(n1 * sizeof(int));
     int *L2 = (int *)malloc(n2 * sizeof(int));
-    for(int i=0;i<n1;i++)
-    {
-        L1[i]=arr[l+i];
+
+    // Copy data to left and right subarrays
+    for (int i = 0; i < n1; i++) {
+        L1[i] = arr[l + i];
     }
-    for(int j=0;j<n2;j++)
-    {
-        L2[j]=arr[(m+1)+j];
+    for (int j = 0; j < n2; j++) {
+        L2[j] = arr[(m + 1) + j];
     }
-    
-   
-    int j=0,i=0,k=l;
-    while(i<n1&&j<n2)
-    {if(L1[i]<=L2[j])
-    {
-        arr[k]=L1[i];
+
+    // Merge the two subarrays back into arr[]
+    int i = 0, j = 0, k = l;  
+    while (i < n1 && j < n2) {
+        if (L1[i] <= L2[j]) {
+            arr[k] = L1[i];
+            i++;
+        } else {
+            arr[k] = L2[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy any remaining elements of L1[], if any
+    while (i < n1) {
+        arr[k] = L1[i];
         i++;
+        k++;
     }
-    else {
-        arr[k]=L2[j];
+
+    // Copy any remaining elements of L2[], if any
+    while (j < n2) {
+        arr[k] = L2[j];
         j++;
+        k++;
     }
-    k++;
-}
-while (i<n1)
-{arr[k]=L1[i];
-    i++;
-k++;}
-while (j<n2)
-{arr[k]=L2[j];
-k++;
-j++;}
-free(L1);
-free(L2);
+
+    // Free allocated memory
+    free(L1);
+    free(L2);
 }
 
-void mergesort(int arr[],int l,int r)
-{if(l<r)
-    { 
-    int m=l+(r-l)/2;
- mergesort(arr,l,m);
- mergesort(arr,m+1,r);
- merge(arr,l,m,r);}}
+// Recursive function to perform Merge Sort
+void mergesort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;  // Find the middle point
 
- void printArray(int A[], int size)
- {
-     int i;
-     for (i = 0; i < size; i++)
-         printf("%d ", A[i]);
-     printf("\n");
- }
+        // Recursively sort first and second halves
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
 
- int main()
- {
-    int arr[] = { 12, 11, 13, 5, 6, 7 };
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
+
+// Function to print an array
+void printArray(int A[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+// Driver code
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
+
     printf("Given array is \n");
     printArray(arr, arr_size);
 
-    mergesort(arr,0,arr_size-1);
+    // Apply Merge Sort
+    mergesort(arr, 0, arr_size - 1);
 
     printf("\nSorted array is \n");
     printArray(arr, arr_size);
+
     return 0;
- }
+}
